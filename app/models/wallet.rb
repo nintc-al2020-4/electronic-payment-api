bd1dbd394ced8ac7db7442d8d12a0604a38c7910
Crow314 <master@crow31415.net>
2021-01-14 13:08:28 +0900

    Update Wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,25 @@
 class Wallet < ApplicationRecord
   belongs_to :user
+
+  validates :balance, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  def add_balance(amount)
+    if amount <= 0
+      raise "Amount must be greater than 0."
+    end
+
+    self.balance += amount
+  end
+
+  def subtract_balance(amount)
+    if amount <= 0
+      raise "Amount must be greater than 0."
+    end
+
+    if self.balance < amount
+      raise "There is NOT enough balance."
+    end
+
+    self.balance -= amount
+  end
 end
