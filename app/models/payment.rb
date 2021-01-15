c2983346acebd458ba8a3afceb9e99820191e16e
Crow314 <master@crow31415.net>
2021-01-16 04:17:49 +0900

    Create rollback process of Refill and Payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,13 +3,19 @@ class Payment < ApplicationRecord
 
   validates :amount, numericality: { greater_than: 0, only_integer: true }
 
-  after_create :pay
+  after_create  :pay
+  after_destroy :rollback
 
 
   private
 
   def pay
     self.wallet.subtract_balance self.amount
+    self.wallet.save!
+  end
+
+  def rollback
+    self.wallet.add_balance self.amount
     self.wallet.save!
   end
 end
--- app/models/refill.rb
+++ app/models/refill.rb
@@ -3,13 +3,19 @@ class Refill < ApplicationRecord
 
   validates :amount, numericality: { greater_than: 0, only_integer: true }
 
-  after_create :refill
+  after_create  :refill
+  after_destroy :rollback
 
 
   private
 
   def refill
     self.wallet.add_balance self.amount
+    self.wallet.save!
+  end
+
+  def rollback
+    self.wallet.subtract_balance self.amount
     self.wallet.save!
   end
 
